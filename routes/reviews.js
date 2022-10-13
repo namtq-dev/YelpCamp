b6410e2faa9356cf3aa339366037200d3004d320
namtq184161 <quangnam260800@gmail.com>
2022-10-13 22:29:49 +0700

    Config express routers

---
+++ routes/reviews.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const catchAsync = require('../helpers/catchAsync');
+const ExpressError = require('../helpers/ExpressError');
+
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+const { reviewSchema } = require('../schemas');
+
+const router = express.Router({ mergeParams: true }); //Router get separate params
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    next();
+}
+
+router.post('/', validateReview, catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+router.delete('/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
+module.exports = router;
